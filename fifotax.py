539b037f2352a53fdcd3598f9406b7408200856a
Konrad <kvisko444@gmail.com>
2021-06-03 14:14:49 +0200

    Add comments

--- fifotax.py
+++ fifotax.py
@@ -1,46 +1,79 @@
 #!/usr/bin/python3
+"""FIFO tax calculator.
 
+This program is used for calculating taxes in first-in-first-out order of buying.
+This program expects a .csv file containing transaction information in format:
+	1. column: date
+	2. column: action (BUY/SELL)
+	3. column: ticker
+	4. column: quantity
+	5. column: price
+
+For example:
+	2021-02-18 BUY PLTR 0.67062549 25.26 16.94
+	- in this case the last columnt (16.94) will get discarded
+"""
 import config
 import logging
 import collections
+import argparse
 from transaction import Transactions
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-def main():
-	transactions = Transactions("taxes.csv")
-	print(transactions)
+def main(args):
+	transactions = Transactions(args.transactions_csv)
+	print(f"Transactions:\n{transactions}")
 
 	state = {}
 	for tr in transactions:
+
+		# If first time seeing the ticker, initialize its state
 		if tr.ticker not in state:
+			logger.info(f"Adding new ticker to state: {tr.ticker}")
 			state[tr.ticker] = {}
 			state[tr.ticker]["buyqueue"] = collections.deque()
 			state[tr.ticker]["gains"] = 0.0
 			state[tr.ticker]["buy_quantity"] = 0.0
 
-		if tr.action == "BUY":
+		# Buy transaction
+		if tr.action.lower() == "buy":
 			state[tr.ticker]["buyqueue"].appendleft(tr)
 			state[tr.ticker]["buy_quantity"] += tr.quantity
 
-		elif tr.action == "SELL":
+		# Sell transaction
+		elif tr.action.lower() == "sell":
 			logger.info("Got a SELL order:")
 			logger.info(tr)
 			logger.info(f"Total buy quantity: {state[tr.ticker]['buy_quantity']}")
+
+			# Do while selling quantity is not depleted
 			while tr.quantity > 0:
 				logger.info(f"{tr.ticker} quantity: {tr.quantity}")
 				buy_trans = state[tr.ticker]["buyqueue"].pop()
 				logger.info(f"Popped: {buy_trans}")
 
-				# buy_trans will get spent
+				# If buy transaction is older than 2y, no tax is paid
+				passed_2y = False
+				time_delta = abs(tr.date - buy_trans.date)
+				logger.info(f"Time delta: {time_delta.days}")
+				if time_delta.days >= 365 * 2:
+					logger.info(f"Time delta is {time_delta} and greater than 2 years, no taxes are paid on it")
+					passed_2y = True
+
+				# If buy_trans will get spent
 				if tr.quantity >= buy_trans.quantity:
-					state[tr.ticker]["gains"] += (tr.price - buy_trans.price)*buy_trans.quantity
+					if not passed_2y:
+						state[tr.ticker]["gains"] += (tr.price - buy_trans.price)*buy_trans.quantity
 					tr.quantity = round(tr.quantity - buy_trans.quantity, 8)
 
-				# buy_trans will get put back in queue
+				# If buy transaction has greater quantity than the current selling,
+				# buy transaction will get put back in queue with reduced quantity
 				elif tr.quantity < buy_trans.quantity:
-					state[tr.ticker]["gains"] += (tr.price - buy_trans.price)*tr.quantity
+					if not passed_2y:
+						state[tr.ticker]["gains"] += (tr.price - buy_trans.price)*tr.quantity
 					buy_trans.quantity -= tr.quantity
 					state[tr.ticker]["buyqueue"].append(buy_trans)
 					tr.quantity -= tr.quantity
@@ -50,13 +83,18 @@ def main():
 		ticker_gains = state[key]['gains']
 		print(f"{key} gains: {ticker_gains}")
 		total_gains += ticker_gains
-
+	if total_gains < 0:
+		logger.info(f"Operating at loss: {total_gains}, no taxes need to be paid")
+		total_gains = 0.0
 	tax = total_gains * config.TAX
 	tax += tax * config.SURTAX
 
 	print(f"Total tax to pay: {tax}")
 
-
 if __name__ == "__main__":
 	logging.basicConfig(format=config.LOG_FORMAT, filename=config.LOG_FILE)
-	main()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("transactions_csv", type=str, help="file containing transactions in CSV format")
+	parser.add_argument("-o", "--output", help="output file")
+	args = parser.parse_args()
+	main(args)
--- transaction.py
+++ transaction.py
@@ -1,11 +1,28 @@
+"""Trasaction module.
+
+This module implements transaction container class and other helper classes.
+	Transactions: container class
+	Transaction: class representing one transaction
+	TransactionParser: class used for parsing transactions CSV file
+"""
+
 import csv
 import config
+import logging
 from datetime import datetime
 
-class Transactions:
-	""" This clas implements a transaction container """
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-	def __init__(self, filename):
+class Transactions:
+	"""This clas implements a transaction container.
+
+	Attributes:
+		tp: transaction parser object
+		transactions: list of transactions parsed from filename
+	"""
+	def __init__(self, filename : str):
+		"""Initializes class with transactions located in CSV file."""
 		self.tp = TransactionParser()
 		self.transactions = self.tp.parse(filename)
 		self.sort()
@@ -20,13 +37,23 @@ class Transactions:
 	def __iter__(self):
 		return iter(self.transactions)
 
-	def sort(self):
+	def sort(self) -> None:
+		"""Sorts transactions by date"""
 		srt = sorted(self.transactions, key=lambda tr: tr.date)
 		self.transactions = srt
 
-class Transaction:
-	""" This class represents a transaction """
 
+class Transaction:
+	"""This class represents a transaction.
+
+	Attributes:
+		date: transaction date
+		action: buy or sell
+		ticker: ticker name
+		quantity: transaction quantity
+		price: price at which trans. is executed
+		total: total transaction cost
+	"""
 	def __init__(self, date : datetime, action : str, ticker : str, quantity : float, price : float):
 		self.date = date
 		self.action = self.validateAction(action)
@@ -38,19 +65,24 @@ class Transaction:
 	def __str__(self):
 		return f"{self.date.date()} {self.action} {self.ticker} {self.quantity} {self.price} {self.total}"
 
-	def validateAction(self, action):
-		if action != "BUY" and action != "SELL":
+	def validateAction(self, action : str) -> str:
+		"""Validates if action is correct and returns it lowercase"""
+		if action.lower() != "buy" and action.lower() != "sell":
 			raise ValueError(f"Action {action} is invalid")
 		else:
-			return action
+			return action.lower()
+
 
 class TransactionParser:
-	""" This class serves as transaction parser from CSV files """
+	"""This class serves as transaction parser from CSV files.
 
+	Attributes:
+		dateformat: format of date to be parsed from CSV file
+	"""
 	def __init__(self, dateformat=config.DATE_FORMAT):
 		self.dateformat = dateformat
 
-	def parse(self, filename):
+	def parse(self, filename : str) -> list:
 		""" Parses CSV transaction data from file """
 		with open(filename, newline="") as csvfile:
 			csvreader = csv.reader(csvfile)
@@ -67,4 +99,5 @@ class TransactionParser:
 				price = float(t[4])
 				transaction = Transaction(date, action, ticker, quantity, price)
 				transactions.append(transaction)
+		logger.info("Transactions parsed successfully")
 		return transactions
